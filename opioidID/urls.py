7b11286e1779936ac5b77aa242cc6183488522f7
Josh Moody <joshmoody24@gmail.com>
2021-12-03 14:27:34 -0700

    changed urls

--- opioidID/urls.py
+++ opioidID/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import indexPageView, searchPrescriberPageView, searchDrugPageView, detailsDrugPageView, detailsPrescriberPageView
 
 urlpatterns = [
-    path('detailsPrescriber/' , detailsPrescriberPageView, name="detailsPerscriber"),
+    path('detailsprescriber/' , detailsPrescriberPageView, name="detailsPerscriber"),
     path('detailsDrug/' , detailsDrugPageView, name="detailsDrug"),
     path('searchDrugs/' , searchDrugPageView, name="searchDrugs" ),
     path('searchPrescribers/' , searchPrescriberPageView, name="searchPerscriber" ),
