4f92263b57194cc94b8b87a5a081467d1c16f474
Josh Moody <joshmoody24@gmail.com>
2021-12-03 14:32:12 -0700

    changed urls and resolved merge conflict

--- opioidID/urls.py
+++ opioidID/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from .views import indexPageView, searchPrescriberPageView, searchDrugPageView, detailsDrugPageView, detailsPrescriberPageView
 
 urlpatterns = [
+<<<<<<< HEAD
     path('detailsPrescriber/' , detailsPrescriberPageView, name="detailsPrescriber"),
+=======
+    path('detailsprescriber/' , detailsPrescriberPageView, name="detailsPerscriber"),
+>>>>>>> 7b11286 (changed urls)
     path('detailsDrug/' , detailsDrugPageView, name="detailsDrug"),
     path('searchDrugs/' , searchDrugPageView, name="searchDrug" ),
     path('searchPrescribers/' , searchPrescriberPageView, name="searchPrescriber" ),
