255caec8d38b316c31d632b47d1431ceaf3122b2
Josh Moody <joshmoody24@gmail.com>
2021-12-03 17:50:39 -0700

    fleshed out search page

--- opioidID/urls.py
+++ opioidID/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import indexPageView, searchPrescriberPageView, searchDrugPageView, detailsDrugPageView, detailsPrescriberPageView
 
 urlpatterns = [
-    path('prescribers/detail/' , detailsPrescriberPageView, name="detailsPrescriber"),
+    path('prescribers/detail/<int:npi>' , detailsPrescriberPageView, name="detailsPrescriber"),
     path('drugs/detail/<str:drug_name>/' , detailsDrugPageView, name="detailsDrug"),
     path('drugs/search/' , searchDrugPageView, name="searchDrug" ),
     path('prescribers/search/' , searchPrescriberPageView, name="searchPrescriber" ),
